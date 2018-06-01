a72fa86970e704fcbef521b18c16ce8453535829
LAPTOP-CNQFB4JI\pikom <piko.mac@gmail.com>
2018-06-01 13:22:52 +0900

    日程調整ツールの縦横切り替えするテーブルをcomponent切り出し

---
+++ src/app/piko-apps/scheduling/answer-page/spreadsheet/date-user-table/date-user-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-user-table',
+  templateUrl: './date-user-table.component.html',
+  styleUrls: ['./date-user-table.component.css']
+})
+export class DateUserTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/piko-apps/scheduling/answer-page/spreadsheet/user-date-table/user-date-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-date-table',
+  templateUrl: './user-date-table.component.html',
+  styleUrls: ['./user-date-table.component.css']
+})
+export class UserDateTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
